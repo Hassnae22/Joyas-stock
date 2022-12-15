42d215598d597c6dc2b5193048d85176a5de2b9d
amina <makkaoui.amina@hotmail.com>
2022-12-15 22:41:42 +0100

    addSwaggerConf

--- src/main/java/com/stock/joyas/controller/indexCtrl.java
+++ src/main/java/com/stock/joyas/controller/indexCtrl.java
@@ -1,5 +1,6 @@
     package com.stock.joyas.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/")
+@Tag(name = "Index", description = "The index API")
 public class indexCtrl {
 
     @GetMapping
